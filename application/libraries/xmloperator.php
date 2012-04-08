3eaf81d4c6de5ad3afa499fa25aa02f38f9def5d
Kemal Kanok <kemalkanok@gmail.com>
2012-04-08 21:54:30 +0300

    new xmlreaderclass init.

--- application/libraries/xmloperator.php
+++ application/libraries/xmloperator.php
@@ -2,17 +2,24 @@
 
 class xmloperator {
 
-//    public function read($filename,$table,$ids,$limit=null)
-//    {
-//    		$doc = new DOMDocument();
-//            $doc->load( $filename );
-//            $books = $doc->getElementsByTagName( $table );
-//			$k=0;
-//			foreach($books as $book)
-//			{
-//				foreach	
-//			}
-//    }
+    public function read($filename,$table,$ids,$limit=null)
+    {
+            $doc = new DOMDocument();
+            $doc->load( $filename );
+            $books = $doc->getElementsByTagName( $table );
+            $k=0;
+            $array=array();
+            foreach($books as $book)
+            {
+                foreach ($ids as $value) 
+                {
+                    $array[$k][$value]=$book->getElementsByTagName( $value )->item(0)->nodeValue;
+                }
+                if($k==$limit)
+                    break;
+                $k++;
+            }
+    }
 }
 
 /* End of file Someclass.php */
