bd6d20a98df1b9da712a3509641c637c97d080fb
Kemal Kanok <kemalkanok@gmail.com>
2012-05-02 01:48:25 +0300

    a lil bug fixed at blog_id

--- application/controllers/welcome.php
+++ application/controllers/welcome.php
@@ -66,7 +66,7 @@ class Welcome extends CI_Controller {
     public function blogentry($id) {
         $array['blog_entries']=array();
         $array["base"]=$_SERVER['SERVER_NAME'];
-        $a = $this->xmloperator->read_all("blog_entries.xml", "blogentry");
+        $a = $this->xmloperator->read_all("blog_entries.xml");
         $array['blog_entries']=$this->xmloperator->search_equal_field($a , "id" , 1);
         foreach ($array['blog_entries'] as $key=>$value) {
              $array['blog_entries'][$key]["body"]=str_replace(array("[", "]"), array("<", ">"), $value["body"]);
